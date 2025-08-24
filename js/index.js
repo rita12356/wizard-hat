26bda5edd5ae25fd29196065e3f3a4d90f5fe5ec
rita12356 <wadeew2016@yandex.ru>
2025-08-24 15:42:10 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function getRandomInt(min, max) {
+    let number = Math.floor(Math.random() * (max - min)) + min;
+    return number;
+}
+
+let hat = document.querySelector(`.hat`);
+let result = document.querySelector(`.result`);
+let messages = [
+  `Добро пожаловать в Гриффиндор!`,
+  `Тебя ждёт большая удача`,
+  `На обед будет что-то невкусное`,
+  `Одевайся теплей`,
+  `Тебя ждёт финансовое благополучие`,
+  `Отлично выспишься сегодня!`,
+  `Домашку всё равно придётся сделать`,
+  `Кто-то сделает тебе комплимент`,
+  `Выходные пройдут отлично`,
+  `Шляпа устала, предсказания не будет`
+  ];
+
+
+
+hat.addEventListener('click', function() {
+  let i = getRandomInt(0, messages.length);
+  let message = messages[i];
+  result.textContent = message;
+
+});
